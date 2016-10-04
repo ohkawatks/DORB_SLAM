96169fe7d1822caed7ed81cbf9ef102ba04595fd
msr <hoge@hoge.com>
2016-10-04 20:31:58 +0900

    first commit

---
+++ src/dataset_reader/src/dataset_reader_node.cpp
@@ -0,0 +1,38 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+
+#include <sstream>
+
+#define PUBLISH_TOPIC_NAME "/camera/rgb/image_raw"
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "talker");
+
+  ros::NodeHandle n;
+  image_transport::ImageTransport it(n);
+  image_transport::Publisher pub_img(it.advertise(PUBLISH_TOPIC_NAME, 1000)); 
+  //ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+  ros::Rate loop_rate(10);
+
+  int count = 0;
+  while (ros::ok())
+  {
+    std_msgs::String msg;
+
+    ROS_INFO("%s", msg.data.c_str());
+    //    chatter_pub.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }
+
+
+  return 0;
+}
